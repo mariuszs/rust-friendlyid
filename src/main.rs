7fd05e16e6c76df4fa106e46e69aba1fc4ee31e4
Mariusz Smykula <mariuszs@gmail.com>
2020-09-06 00:32:33 +0200

    add create id

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use clap::{crate_version, crate_authors, App, Arg};
-use uuid::Uuid;
+use clap::{App, Arg, crate_authors, crate_version};
 use friendly_id;
-
+use uuid::Uuid;
 
 fn main() {
     let matches = App::new("FriendlyId Converter")
@@ -10,7 +9,7 @@ fn main() {
         .about("The FriendlyID library converts a given UUID to a URL-friendly ID which is based on Base62")
         .arg(Arg::with_name("ID")
             .help("UUID to convert or ID to decode")
-            .required(true)
+            .required(false)
             .index(1))
         .arg(Arg::with_name("decode")
             .short("d")
@@ -19,16 +18,19 @@ fn main() {
             .help("Decode friendlyId"))
         .get_matches();
 
-    let id = matches.value_of("ID").expect("Missing id!");
-
-    if matches.is_present("decode") {
-        println!("{}", friendly_id::decode(id.to_string()));
-    } else {
-        if id.contains("-") {
-            let my_uuid = Uuid::parse_str(id).expect("Invalid uuid format!");
-            println!("{}", friendly_id::encode(&my_uuid));
-        } else {
-            println!("{}", friendly_id::decode(id.to_string()));
+    match matches.value_of("ID") {
+        None => println!("{}", friendly_id::create()),
+        Some(id) => {
+            if matches.is_present("decode") {
+                println!("{}", friendly_id::decode(id.to_string()));
+            } else {
+                if id.contains("-") {
+                    let my_uuid = Uuid::parse_str(id).expect("Invalid uuid format!");
+                    println!("{}", friendly_id::encode(&my_uuid));
+                } else {
+                    println!("{}", friendly_id::decode(id.to_string()));
+                }
+            }
         }
     }
 }
